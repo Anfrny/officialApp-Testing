8a269cd3f7b0113d79e184ff813f22d98213493f
Anfrny <anferneeviduya@gmail.com>
2018-10-08 22:29:54 -0700

    Commiting entire app for testing.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,48 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import  firebase  from 'firebase';
+import { firebaseDb } from 'firebase/database'
+
+var config = {
+  apiKey: "AIzaSyApV_GjzxUqHN0hhZ7hp634fm3agbZUWcY",
+  authDomain: "acis-676.firebaseapp.com",
+  databaseURL: "https://acis-676.firebaseio.com",
+  projectId: "acis-676",
+  storageBucket: "acis-676.appspot.com",
+  messagingSenderId: "229859729202"
+};
+firebase.initializeApp(config)
+
+//var ocarm = document.getElementById('carm_10_s')
+//const carmdbRef_10: firebaseDb.Reference = firebase.database().ref('/Main/').child('/Signature/').child('/Carmela/').child('/carm-10/');
+//carmdbRef_10.on('value', snap => ocarm.innerHTML = snap.val());
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+
+export class HomePage {
+ //public carm_Ref_10  = {};
+  //public carm_Ref_16 = {};
+
+
+
+  constructor() {
+
+
+    //const carmdbRef_10: firebaseDb.Reference = firebase.database().ref('/Main/').child('/Signature/').child('/Carmela/').child('/carm-10/');
+   // carmdbRef_10.on('value', snap => this.carm_Ref_10 = snap.val());
+
+   // const carmRef_16: firebaseDb.Reference = firebase.database().ref('Main').child('Signature').child('Carmela').child('carm-16');
+   // carmRef_16.on('value', carmSnapshot => this.carm_Ref_16 = carmSnapshot.val());
+
 
   }
 
+
+
+
 }
+
+
